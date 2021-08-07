cf1aa1eef9f315107a32b43cf71a8caab2c07578
RafayShaikh <rafaysarr@gmail.com>
2021-08-07 12:10:49 -0500

    Functionalities v2.0.1

--- wheelwise/pages/api/questions.js
+++ wheelwise/pages/api/questions.js
@@ -22,7 +22,11 @@ export default function handler(req, res) {
     html: message.replace(/\r\n/g, '<br>'),
   };
 
-  mail.send(data);
+  try {
+    mail.send(data);
+  } catch (error) {
+    console.log(error);
+  }
 
   res.status(200).json({ status: 'ok' });
 }
